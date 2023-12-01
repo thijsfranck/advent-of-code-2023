f0fdf10e04c5c1dc0cbdb190615f9762454da58e
thijsfranck <thijsfranck@gmail.com>
2023-12-01 07:26:04 +0100

    Fix docstring

--- advent_of_code_2023/day1/test__part2.py
+++ advent_of_code_2023/day1/test__part2.py
@@ -10,5 +10,5 @@ def test__sum_calibration_values_with_test_input() -> None:
     assert sum_calibration_values(Path(__file__).parent / "input/test__input_part2") == 281
 
 def test__sum_calibration_values_with_puzzle_input() -> None:
-    """Test whether the sum of all calibration values matches the example solution."""
+    """Test whether the sum of all calibration values matches the puzzle solution."""
     assert sum_calibration_values(Path(__file__).parent / "input/input") == 54824
