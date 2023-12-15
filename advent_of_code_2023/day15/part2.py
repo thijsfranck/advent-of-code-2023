a6121168ab539547d077bc3f9034eb8c6f2b39de
thijsfranck <thijsfranck@gmail.com>
2023-12-15 09:14:52 +0000

    simplify part 2

--- advent_of_code_2023/day15/part2.py
+++ advent_of_code_2023/day15/part2.py
@@ -1,12 +1,12 @@
 """Advent of Code 2023 Day 15 Part 2."""
 
-import contextlib
 import logging
 from collections import defaultdict
 from collections.abc import Generator, Iterable
-from dataclasses import dataclass
 from functools import reduce
 from pathlib import Path
+
+Step = tuple[str, str, int]
 
 
 def read_char_stream(path: Path) -> Generator[str, None, None]:
@@ -14,39 +14,6 @@ def read_char_stream(path: Path) -> Generator[str, None, None]:
     with Path.open(path) as f:
         while char := f.read(1):
             yield char
-
-
-@dataclass(frozen=True)
-class Step:
-    """A step in the initialization sequence."""
-
-    label: str
-    operator: str
-    focal_length: int = 0
-
-    def __hash__(self) -> int:
-        return reduce(
-            lambda result, char: ((result + ord(char)) * 17) % 256,
-            self.label,
-            0,
-        )
-
-    def __eq__(self, other: "Step") -> bool:
-        return self.label == other.label
-
-    def apply(self, box: list["Step"]) -> None:
-        """Apply the step to the box."""
-        if self.operator == "-":
-            try:
-                box.remove(self)
-            except ValueError:
-                contextlib.suppress(ValueError)
-            return
-        try:
-            index = box.index(self)
-            box[index] = self
-        except ValueError:
-            box.append(self)
 
 
 def parse_initialization_sequences(stream: Iterable[str]) -> Generator[Step, None, None]:
@@ -57,7 +24,7 @@ def parse_initialization_sequences(stream: Iterable[str]) -> Generator[Step, Non
 
     for char in stream:
         if char in {"\n", ","}:
-            yield Step(
+            yield (
                 current_label,
                 current_operator,
                 int(current_focal_length) if current_focal_length else 0,
@@ -80,17 +47,31 @@ def parse_initialization_sequences(stream: Iterable[str]) -> Generator[Step, Non
             continue
 
 
+def calculate_hash(initialization_sequence: str) -> int:
+    """Calculate the hash of an initialization sequence."""
+    return reduce(
+        lambda result, char: ((result + ord(char)) * 17) % 256,
+        initialization_sequence,
+        0,
+    )
+
+
 def calculate_solution(path: Path) -> int:
     """Calculate the solution."""
-    boxes: dict[int, list[Step]] = defaultdict(list)
+    boxes: dict[int, dict[str, int]] = defaultdict(dict)
 
-    for step in parse_initialization_sequences(read_char_stream(path)):
-        step.apply(boxes[hash(step)])
+    for label, operator, focal_length in parse_initialization_sequences(read_char_stream(path)):
+        label_hash = calculate_hash(label)
+        box = boxes[label_hash]
+        if operator == "=":
+            box[label] = focal_length
+        elif operator == "-" and label in box:
+            del box[label]
 
     return sum(
-        (1 + box) * (1 + slot) * lens.focal_length
+        (1 + box) * (1 + slot) * focal_length
         for box, lenses in boxes.items()
-        for slot, lens in enumerate(lenses)
+        for slot, focal_length in enumerate(lenses.values())
     )
 
 
